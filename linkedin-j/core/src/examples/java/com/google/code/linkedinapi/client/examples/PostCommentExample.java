0d31956f6b7abdc8571197f1832c0dca12fd5952
Vamsi Krishna Sankarayogi <vsankarayogi@gmail.com>
2009-12-03 15:22:11 +0000

    Initial commit of linkedin-j API client.

---
+++ linkedin-j/core/src/examples/java/com/google/code/linkedinapi/client/examples/PostCommentExample.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.google.code.linkedinapi.client.examples;
+
+/**
+ * @author nmukhtar
+ *
+ */
+public class PostCommentExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ linkedin-j/core/src/examples/java/com/google/code/linkedinapi/client/examples/ProfileApiExample.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.google.code.linkedinapi.client.examples;
+
+/**
+ * @author nmukhtar
+ *
+ */
+public class ProfileApiExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
