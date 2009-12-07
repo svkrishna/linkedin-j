96552655b31453b1ebb7bfd8523fcf56cf80311d
Vamsi Krishna Sankarayogi <vsankarayogi@gmail.com>
2009-12-07 07:29:08 +0000

    Fixed some command line examples.

--- linkedin-j/core/src/examples/java/com/google/code/linkedinapi/client/examples/PostCommentExample.java
+++ linkedin-j/core/src/examples/java/com/google/code/linkedinapi/client/examples/PostCommentExample.java
@@ -1,9 +1,7 @@
 /**
- * 
+ *
  */
 package com.google.code.linkedinapi.client.examples;
-
-import java.text.MessageFormat;
 
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
@@ -15,7 +13,6 @@ import org.apache.commons.cli.ParseException;
 
 import com.google.code.linkedinapi.client.LinkedInApiClient;
 import com.google.code.linkedinapi.client.LinkedInApiClientFactory;
-import com.google.code.linkedinapi.schema.Connections;
 
 /**
  * @author nmukhtar
@@ -27,42 +24,37 @@ public class PostCommentExample {
      * Consumer Key
      */
     private static final String CONSUMER_KEY_OPTION = "consumerKey";
-	
+
     /**
      * Consumer Secret
      */
     private static final String CONSUMER_SECRET_OPTION = "consumerSecret";
-    
+
     /**
      * Access Token
      */
     private static final String ACCESS_TOKEN_OPTION = "token";
-	
+
     /**
      * Access Token Secret
      */
     private static final String ACCESS_TOKEN_SECRET_OPTION = "tokenSecret";
 
     /**
-     * ID
+     * Network Update ID
      */
-    private static final String ID_OPTION = "id";
-    
+    private static final String POST_ID_OPTION = "postId";
+
     /**
-     * Email
+     * Comment Text
      */
-    private static final String EMAIL_OPTION = "email";
-    
-    /**
-     * URL
-     */
-    private static final String URL_OPTION = "url";
-    
+    private static final String COMMENT_TEXT_OPTION = "comment";
+
     /**
      * Name of the help command line option.
      */
     private static final String HELP_OPTION = "help";
-    
+
     /**
 	 * @param args
 	 */
@@ -76,55 +68,41 @@ public class PostCommentExample {
             printHelp(options);
         }
 	}
-	
+
     /**
-     * Process command line options and call the service. 
+     * Process command line options and call the service.
      */
     private static void processCommandLine(CommandLine line, Options options) {
         if(line.hasOption(HELP_OPTION)) {
-            printHelp(options);            
+            printHelp(options);
         } else if(line.hasOption(CONSUMER_KEY_OPTION) && line.hasOption(CONSUMER_SECRET_OPTION)
-        		&& line.hasOption(ACCESS_TOKEN_OPTION) && line.hasOption(ACCESS_TOKEN_SECRET_OPTION)) {
+        		&& line.hasOption(ACCESS_TOKEN_OPTION) && line.hasOption(ACCESS_TOKEN_SECRET_OPTION)
+        		&& line.hasOption(POST_ID_OPTION) && line.hasOption(COMMENT_TEXT_OPTION)) {
     		final String consumerKeyValue = line.getOptionValue(CONSUMER_KEY_OPTION);
     		final String consumerSecretValue = line.getOptionValue(CONSUMER_SECRET_OPTION);
     		final String accessTokenValue = line.getOptionValue(ACCESS_TOKEN_OPTION);
     		final String tokenSecretValue = line.getOptionValue(ACCESS_TOKEN_SECRET_OPTION);
-    		
+    		final String postId = line.getOptionValue(POST_ID_OPTION);
+    		final String comment = line.getOptionValue(COMMENT_TEXT_OPTION);
+
     		final LinkedInApiClientFactory factory = LinkedInApiClientFactory.newInstance(consumerKeyValue, consumerSecretValue);
     		final LinkedInApiClient client = factory.createLinkedInApiClient(accessTokenValue, tokenSecretValue);
-    		
-    		if(line.hasOption(ID_OPTION)) {
-    			String idValue = line.getOptionValue(ID_OPTION);
-    			System.out.println("Fetching connections for user with id:" + idValue);
-    			Connections connections = client.getConnectionsById(idValue);
-    			printResult(connections);
-    		} else if (line.hasOption(EMAIL_OPTION)) {
-    			String emailValue = line.getOptionValue(EMAIL_OPTION);
-    			System.out.println("Fetching connections for user with email:" + emailValue);
-    			Connections connections = client.getConnectionsByEmail(emailValue);
-    			printResult(connections);
-    		} else if (line.hasOption(URL_OPTION)) {
-    			String urlValue = line.getOptionValue(URL_OPTION);
-    			System.out.println("Fetching connections for user with url:" + urlValue);
-    			Connections connections = client.getConnectionsByUrl(urlValue);
-    			printResult(connections);
-    		} else {
-    			System.out.println("Fetching connections for current user.");
-    			Connections connections = client.getConnectionsForCurrentUser();
-    			printResult(connections);
-    		}
+
+    		client.postComment(postId, comment);
+
+    		System.out.println("The comment has been posted successfully. Check the LinkedIn site for confirmation.");
         } else {
             printHelp(options);
         }
     }
-	
+
 	/**
      * Build command line options object.
      */
     private static Options buildOptions() {
-       
+
         Options opts = new Options();
-        
+
         String helpMsg = "Print this message.";
         Option help = new Option(HELP_OPTION, helpMsg);
         opts.addOption(help);
@@ -135,67 +113,52 @@ public class PostCommentExample {
         OptionBuilder.withDescription(consumerKeyMsg);
         Option consumerKey = OptionBuilder.create(CONSUMER_KEY_OPTION);
         opts.addOption(consumerKey);
-        
+
         String consumerSecretMsg = "You API Consumer Secret.";
         OptionBuilder.withArgName("consumerSecret");
         OptionBuilder.hasArg();
         OptionBuilder.withDescription(consumerSecretMsg);
         Option consumerSecret = OptionBuilder.create(CONSUMER_SECRET_OPTION);
         opts.addOption(consumerSecret);
-        
+
         String accessTokenMsg = "You OAuth Access Token.";
         OptionBuilder.withArgName("accessToken");
         OptionBuilder.hasArg();
         OptionBuilder.withDescription(accessTokenMsg);
         Option accessToken = OptionBuilder.create(ACCESS_TOKEN_OPTION);
         opts.addOption(accessToken);
-        
+
         String tokenSecretMsg = "You OAuth Access Token Secret.";
         OptionBuilder.withArgName("accessTokenSecret");
         OptionBuilder.hasArg();
         OptionBuilder.withDescription(tokenSecretMsg);
         Option accessTokenSecret = OptionBuilder.create(ACCESS_TOKEN_SECRET_OPTION);
         opts.addOption(accessTokenSecret);
-        
-        String idMsg = "ID of the user whose connections are to be fetched.";
-        OptionBuilder.withArgName("id");
+
+        String idMsg = "ID of the post on which to comment.";
+        OptionBuilder.withArgName("postId");
         OptionBuilder.hasArg();
         OptionBuilder.withDescription(idMsg);
-        Option id = OptionBuilder.create(ID_OPTION);
+        Option id = OptionBuilder.create(POST_ID_OPTION);
         opts.addOption(id);
-        
-        String emailMsg = "Email of the user whose connections are to be fetched.";
-        OptionBuilder.withArgName("email");
+
+        String commentMsg = "Text of the comment.";
+        OptionBuilder.withArgName("comment");
         OptionBuilder.hasArg();
-        OptionBuilder.withDescription(emailMsg);
-        Option email = OptionBuilder.create(EMAIL_OPTION);
-        opts.addOption(email);
-        
-        String urlMsg = "Profile URL of the user whose connections are to be fetched.";
-        OptionBuilder.withArgName("url");
-        OptionBuilder.hasArg();
-        OptionBuilder.withDescription(urlMsg);
-        Option url = OptionBuilder.create(URL_OPTION);
-        opts.addOption(url);
-        
+        OptionBuilder.withDescription(commentMsg);
+        Option comment = OptionBuilder.create(COMMENT_TEXT_OPTION);
+        opts.addOption(comment);
+
         return opts;
     }
-    
+
     /**
      * Print help and usage.
      */
     private static void printHelp(Options options) {
         int width = 80;
         String syntax = PostCommentExample.class.getName() + " <options>";
-        String header = MessageFormat.format("\nThe -{0}, -{1}, -{2} and -{3} options are required. All others are optional.", CONSUMER_KEY_OPTION, CONSUMER_SECRET_OPTION, ACCESS_TOKEN_OPTION, ACCESS_TOKEN_SECRET_OPTION);
-        String footer = MessageFormat.format("\nIf you do not specify any of -{0}, -{1} or -{2} options, the connections of current user are returned. You can only specify one of these options.", ID_OPTION, EMAIL_OPTION, URL_OPTION);
-        new HelpFormatter().printHelp(width, syntax, header, options, footer, false);
+        String header = "\nAll options are required. Post ID must be a valid network update ID.";
+        new HelpFormatter().printHelp(width, syntax, header, options, null, false);
     }
-    
-    /**
-     * Print the result of API call.
-     */
-    private static void printResult(Connections connections) {
-		// TODO Auto-generated method stub
-	}
 }
--- linkedin-j/core/src/examples/java/com/google/code/linkedinapi/client/examples/PostNetworkUpdateExample.java
+++ linkedin-j/core/src/examples/java/com/google/code/linkedinapi/client/examples/PostNetworkUpdateExample.java
@@ -1,9 +1,7 @@
 /**
- * 
+ *
  */
 package com.google.code.linkedinapi.client.examples;
-
-import java.text.MessageFormat;
 
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
@@ -15,7 +13,6 @@ import org.apache.commons.cli.ParseException;
 
 import com.google.code.linkedinapi.client.LinkedInApiClient;
 import com.google.code.linkedinapi.client.LinkedInApiClientFactory;
-import com.google.code.linkedinapi.schema.Connections;
 
 /**
  * @author nmukhtar
@@ -27,42 +24,32 @@ public class PostNetworkUpdateExample {
      * Consumer Key
      */
     private static final String CONSUMER_KEY_OPTION = "consumerKey";
-	
+
     /**
      * Consumer Secret
      */
     private static final String CONSUMER_SECRET_OPTION = "consumerSecret";
-    
+
     /**
      * Access Token
      */
     private static final String ACCESS_TOKEN_OPTION = "token";
-	
+
     /**
      * Access Token Secret
      */
     private static final String ACCESS_TOKEN_SECRET_OPTION = "tokenSecret";
 
     /**
-     * ID
+     * Status Text
      */
-    private static final String ID_OPTION = "id";
-    
-    /**
-     * Email
-     */
-    private static final String EMAIL_OPTION = "email";
-    
-    /**
-     * URL
-     */
-    private static final String URL_OPTION = "url";
-    
+    private static final String UPDATE_TEXT_OPTION = "update";
+
     /**
      * Name of the help command line option.
      */
     private static final String HELP_OPTION = "help";
-    
+
     /**
 	 * @param args
 	 */
@@ -76,55 +63,40 @@ public class PostNetworkUpdateExample {
             printHelp(options);
         }
 	}
-	
+
     /**
-     * Process command line options and call the service. 
+     * Process command line options and call the service.
      */
     private static void processCommandLine(CommandLine line, Options options) {
         if(line.hasOption(HELP_OPTION)) {
-            printHelp(options);            
+            printHelp(options);
         } else if(line.hasOption(CONSUMER_KEY_OPTION) && line.hasOption(CONSUMER_SECRET_OPTION)
         		&& line.hasOption(ACCESS_TOKEN_OPTION) && line.hasOption(ACCESS_TOKEN_SECRET_OPTION)) {
     		final String consumerKeyValue = line.getOptionValue(CONSUMER_KEY_OPTION);
     		final String consumerSecretValue = line.getOptionValue(CONSUMER_SECRET_OPTION);
     		final String accessTokenValue = line.getOptionValue(ACCESS_TOKEN_OPTION);
     		final String tokenSecretValue = line.getOptionValue(ACCESS_TOKEN_SECRET_OPTION);
-    		
+
     		final LinkedInApiClientFactory factory = LinkedInApiClientFactory.newInstance(consumerKeyValue, consumerSecretValue);
     		final LinkedInApiClient client = factory.createLinkedInApiClient(accessTokenValue, tokenSecretValue);
-    		
-    		if(line.hasOption(ID_OPTION)) {
-    			String idValue = line.getOptionValue(ID_OPTION);
-    			System.out.println("Fetching connections for user with id:" + idValue);
-    			Connections connections = client.getConnectionsById(idValue);
-    			printResult(connections);
-    		} else if (line.hasOption(EMAIL_OPTION)) {
-    			String emailValue = line.getOptionValue(EMAIL_OPTION);
-    			System.out.println("Fetching connections for user with email:" + emailValue);
-    			Connections connections = client.getConnectionsByEmail(emailValue);
-    			printResult(connections);
-    		} else if (line.hasOption(URL_OPTION)) {
-    			String urlValue = line.getOptionValue(URL_OPTION);
-    			System.out.println("Fetching connections for user with url:" + urlValue);
-    			Connections connections = client.getConnectionsByUrl(urlValue);
-    			printResult(connections);
-    		} else {
-    			System.out.println("Fetching connections for current user.");
-    			Connections connections = client.getConnectionsForCurrentUser();
-    			printResult(connections);
+
+    		if (line.hasOption(UPDATE_TEXT_OPTION)) {
+        		final String updateText = line.getOptionValue(UPDATE_TEXT_OPTION);
+        		client.postNetworkUpdate(updateText);
+        		System.out.println("Your update has been posted. Check the LinkedIn site for confirmation.");
     		}
         } else {
             printHelp(options);
         }
     }
-	
+
 	/**
      * Build command line options object.
      */
     private static Options buildOptions() {
-       
+
         Options opts = new Options();
-        
+
         String helpMsg = "Print this message.";
         Option help = new Option(HELP_OPTION, helpMsg);
         opts.addOption(help);
@@ -135,67 +107,45 @@ public class PostNetworkUpdateExample {
         OptionBuilder.withDescription(consumerKeyMsg);
         Option consumerKey = OptionBuilder.create(CONSUMER_KEY_OPTION);
         opts.addOption(consumerKey);
-        
+
         String consumerSecretMsg = "You API Consumer Secret.";
         OptionBuilder.withArgName("consumerSecret");
         OptionBuilder.hasArg();
         OptionBuilder.withDescription(consumerSecretMsg);
         Option consumerSecret = OptionBuilder.create(CONSUMER_SECRET_OPTION);
         opts.addOption(consumerSecret);
-        
+
         String accessTokenMsg = "You OAuth Access Token.";
         OptionBuilder.withArgName("accessToken");
         OptionBuilder.hasArg();
         OptionBuilder.withDescription(accessTokenMsg);
         Option accessToken = OptionBuilder.create(ACCESS_TOKEN_OPTION);
         opts.addOption(accessToken);
-        
+
         String tokenSecretMsg = "You OAuth Access Token Secret.";
         OptionBuilder.withArgName("accessTokenSecret");
         OptionBuilder.hasArg();
         OptionBuilder.withDescription(tokenSecretMsg);
         Option accessTokenSecret = OptionBuilder.create(ACCESS_TOKEN_SECRET_OPTION);
         opts.addOption(accessTokenSecret);
-        
-        String idMsg = "ID of the user whose connections are to be fetched.";
-        OptionBuilder.withArgName("id");
+
+        String updateMsg = "Text of the update.";
+        OptionBuilder.withArgName("update");
         OptionBuilder.hasArg();
-        OptionBuilder.withDescription(idMsg);
-        Option id = OptionBuilder.create(ID_OPTION);
-        opts.addOption(id);
-        
-        String emailMsg = "Email of the user whose connections are to be fetched.";
-        OptionBuilder.withArgName("email");
-        OptionBuilder.hasArg();
-        OptionBuilder.withDescription(emailMsg);
-        Option email = OptionBuilder.create(EMAIL_OPTION);
-        opts.addOption(email);
-        
-        String urlMsg = "Profile URL of the user whose connections are to be fetched.";
-        OptionBuilder.withArgName("url");
-        OptionBuilder.hasArg();
-        OptionBuilder.withDescription(urlMsg);
-        Option url = OptionBuilder.create(URL_OPTION);
-        opts.addOption(url);
-        
+        OptionBuilder.withDescription(updateMsg);
+        Option update = OptionBuilder.create(UPDATE_TEXT_OPTION);
+        opts.addOption(update);
+
         return opts;
     }
-    
+
     /**
      * Print help and usage.
      */
     private static void printHelp(Options options) {
         int width = 80;
         String syntax = PostNetworkUpdateExample.class.getName() + " <options>";
-        String header = MessageFormat.format("\nThe -{0}, -{1}, -{2} and -{3} options are required. All others are optional.", CONSUMER_KEY_OPTION, CONSUMER_SECRET_OPTION, ACCESS_TOKEN_OPTION, ACCESS_TOKEN_SECRET_OPTION);
-        String footer = MessageFormat.format("\nIf you do not specify any of -{0}, -{1} or -{2} options, the connections of current user are returned. You can only specify one of these options.", ID_OPTION, EMAIL_OPTION, URL_OPTION);
-        new HelpFormatter().printHelp(width, syntax, header, options, footer, false);
+        String header = "\nAll options are required.";
+        new HelpFormatter().printHelp(width, syntax, header, options, null, false);
     }
-    
-    /**
-     * Print the result of API call.
-     */
-    private static void printResult(Connections connections) {
-		// TODO Auto-generated method stub
-	}
 }
