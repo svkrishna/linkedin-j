c744b6fd8f3be93d595f5db9e646db1599028457
Vamsi Krishna Sankarayogi <vsankarayogi@gmail.com>
2009-12-15 07:36:39 +0000

    Fixed the issue with comments in network updates.

---
+++ linkedin-j/schema/src/main/java/com/google/code/linkedinapi/schema/dom/UpdateCommentsImpl.java
@@ -0,0 +1,56 @@
+
+package com.google.code.linkedinapi.schema.dom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import com.google.code.linkedinapi.schema.UpdateComment;
+import com.google.code.linkedinapi.schema.UpdateComments;
+
+public class UpdateCommentsImpl
+	extends BaseSchemaEntity
+    implements UpdateComments
+{
+
+    protected List<UpdateComment> updateComment;
+    protected Long total;
+
+    public List<UpdateComment> getUpdateComment() {
+        if (updateComment == null) {
+            updateComment = new ArrayList<UpdateComment>();
+        }
+        return this.updateComment;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long value) {
+        this.total = value;
+    }
+    
+	@Override
+	public void init(Element element) {
+		setTotal(DomUtils.getAttributeValueAsLongFromNode(element, "total"));
+		List<Element> updateComments = DomUtils.getChildElementsByLocalName(element, "update-comment");
+		for (Element updateComment : updateComments) {
+			UpdateCommentImpl updateCommentImpl = new UpdateCommentImpl();
+			updateCommentImpl.init(updateComment);
+			getUpdateComment().add(updateCommentImpl);
+		}
+	}
+
+	@Override
+	public Element toXml(Document document) {
+		Element element = document.createElement("update-comments");
+		DomUtils.setAttributeValueToNode(element, "total", getTotal());
+		for (UpdateComment updateComment : getUpdateComment()) {
+			element.appendChild(((UpdateCommentImpl) updateComment).toXml(document));
+		}
+		return element;
+	}
+}
