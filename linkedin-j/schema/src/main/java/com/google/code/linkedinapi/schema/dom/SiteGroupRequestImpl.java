35eba3133e63e42043acf95378c7f1f97928277c
Vamsi Krishna Sankarayogi <vsankarayogi@gmail.com>
2009-12-30 10:04:13 +0000

    Added more update types to network updates. Commit is currently broken and is being fixed.

--- linkedin-j/schema/src/main/java/com/google/code/linkedinapi/schema/dom/SiteGroupRequestImpl.java
+++ linkedin-j/schema/src/main/java/com/google/code/linkedinapi/schema/dom/SiteGroupRequestImpl.java
@@ -1,6 +1,9 @@
 
 package com.google.code.linkedinapi.schema.dom;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 import com.google.code.linkedinapi.schema.Headers;
 import com.google.code.linkedinapi.schema.SiteGroupRequest;
 
@@ -28,5 +31,24 @@ public class SiteGroupRequestImpl
     public void setHeaders(Headers value) {
         this.headers = ((HeadersImpl) value);
     }
+    
+	@Override
+	public void init(Element element) {
+		setUrl(DomUtils.getElementValueFromNode(element, "url"));
+		Element headersElem = (Element) DomUtils.getChildElementByName(element, "headers");
+		if (headersElem != null) {
+			HeadersImpl headerImpl = new HeadersImpl();
+			headerImpl.init(headersElem);
+		}
+	}
 
+	@Override
+	public Element toXml(Document document) {
+		Element element = document.createElement("site-group-request");
+		DomUtils.setElementValueToNode(element, "url", getUrl());
+		if (getHeaders() != null) {
+			element.appendChild(((HeadersImpl) getHeaders()).toXml(document));
+		}
+		return element;
+	}
 }
