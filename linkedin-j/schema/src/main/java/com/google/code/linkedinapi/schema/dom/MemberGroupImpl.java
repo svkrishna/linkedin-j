cc949d09fcf6181ee0599869ba84314b007fb20d
Vamsi Krishna Sankarayogi <vsankarayogi@gmail.com>
2009-12-30 10:34:57 +0000

    Added more update types to network updates. Commit is currently broken and is being fixed.

--- linkedin-j/schema/src/main/java/com/google/code/linkedinapi/schema/dom/MemberGroupImpl.java
+++ linkedin-j/schema/src/main/java/com/google/code/linkedinapi/schema/dom/MemberGroupImpl.java
@@ -1,6 +1,9 @@
 
 package com.google.code.linkedinapi.schema.dom;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 import com.google.code.linkedinapi.schema.MemberGroup;
 import com.google.code.linkedinapi.schema.SiteGroupRequest;
 
@@ -37,5 +40,28 @@ public class MemberGroupImpl
     public void setSiteGroupRequest(SiteGroupRequest value) {
         this.siteGroupRequest = ((SiteGroupRequestImpl) value);
     }
+    
+	@Override
+	public void init(Element element) {
+		setId(DomUtils.getElementValueFromNode(element, "id"));
+		setName(DomUtils.getElementValueFromNode(element, "name"));
+		
+		Element siteGroupElem = (Element) DomUtils.getChildElementByName(element, "site-group-request");
+		if (siteGroupElem != null) {
+			SiteGroupRequestImpl siteGroup = new SiteGroupRequestImpl();
+			siteGroup.init(siteGroupElem);
+			setSiteGroupRequest(siteGroup);
+		}
+	}
 
+	@Override
+	public Element toXml(Document document) {
+		Element element = document.createElement("member-group");
+		DomUtils.setElementValueToNode(element, "id", getId());
+		DomUtils.setElementValueToNode(element, "name", getName());
+		if (getSiteGroupRequest() != null) {
+			element.appendChild(((SiteGroupRequestImpl) getSiteGroupRequest()).toXml(document));
+		}
+		return element;
+	}
 }
