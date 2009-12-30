87fd541d43eebb34cfadd4e0a0efc78f28b06c93
Vamsi Krishna Sankarayogi <vsankarayogi@gmail.com>
2009-12-30 10:18:35 +0000

    Added more update types to network updates. Commit is currently broken and is being fixed.

--- linkedin-j/schema/src/main/java/com/google/code/linkedinapi/schema/dom/PersonActivitiesImpl.java
+++ linkedin-j/schema/src/main/java/com/google/code/linkedinapi/schema/dom/PersonActivitiesImpl.java
@@ -3,8 +3,11 @@ package com.google.code.linkedinapi.schema.dom;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 import com.google.code.linkedinapi.schema.Activity;
-import com.google.code.linkedinapi.schema.Adapter1;
 import com.google.code.linkedinapi.schema.PersonActivities;
 
 public class PersonActivitiesImpl
@@ -31,4 +34,24 @@ public class PersonActivitiesImpl
         this.count = value;
     }
 
+	@Override
+	public void init(Element element) {
+		setCount(DomUtils.getAttributeValueAsLongFromNode(element, "count"));
+		List<Element> activities = DomUtils.getChildElementsByLocalName(element, "activity");
+		for (Element education : activities) {
+			ActivityImpl activityImpl = new ActivityImpl();
+			activityImpl.init(education);
+			getActivity().add(activityImpl);
+		}
+	}
+
+	@Override
+	public Element toXml(Document document) {
+		Element element = document.createElement("person-activities");
+		DomUtils.setAttributeValueToNode(element, "count", getCount());
+		for (Activity activity : getActivity()) {
+			element.appendChild(((ActivityImpl) activity).toXml(document));
+		}
+		return element;
+	}
 }
