2f941cbaf2c7530b009fb9306f77a26b9e5f323a
Vamsi Krishna Sankarayogi <vsankarayogi@gmail.com>
2010-03-11 15:46:27 +0000

    Asynchronous operations..

--- linkedin-j/core/src/main/java/com/google/code/linkedinapi/client/AsyncResponseHandler.java
+++ linkedin-j/core/src/main/java/com/google/code/linkedinapi/client/AsyncResponseHandler.java
@@ -3,21 +3,42 @@
  */
 package com.google.code.linkedinapi.client;
 
+import java.util.concurrent.Future;
+
 import com.google.code.linkedinapi.schema.SchemaEntity;
 
 /**
- * The Interface AsyncResponseHandler.
+ * The Abstract class AsyncResponseHandler.
  *
  * @author nmukhtar
  *
  * @param <T>
  */
-public interface AsyncResponseHandler<T extends SchemaEntity> {
+public abstract class AsyncResponseHandler<T extends SchemaEntity> {
+	
+	private Future<T> future;
+	
+    /**
+     * Set future
+     *
+     * @param future the future
+     */
+	public void setFuture(Future<T> future) {
+		this.future = future;
+	}
+	
+    /**
+     * Get a reference to the future.
+     *
+     */
+	public Future<T> getFuture() {
+		return future;
+	}
 
     /**
      * Handle response.
      *
      * @param response the response
      */
-    public void handleResponse(T response);
+    public abstract void handleResponse(T response);
 }
