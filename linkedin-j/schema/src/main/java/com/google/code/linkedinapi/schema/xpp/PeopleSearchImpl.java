136ea93873212faba4b26c0afa7982ab04c54c07
Vamsi Krishna Sankarayogi <vsankarayogi@gmail.com>
2010-08-07 13:13:57 +0000

    Facets....

--- linkedin-j/schema/src/main/java/com/google/code/linkedinapi/schema/xpp/PeopleSearchImpl.java
+++ linkedin-j/schema/src/main/java/com/google/code/linkedinapi/schema/xpp/PeopleSearchImpl.java
@@ -17,6 +17,12 @@
 
 package com.google.code.linkedinapi.schema.xpp;
 
+import java.io.IOException;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlSerializer;
+
 import com.google.code.linkedinapi.schema.Facets;
 import com.google.code.linkedinapi.schema.People;
 import com.google.code.linkedinapi.schema.PeopleSearch;
@@ -55,4 +61,41 @@ public class PeopleSearchImpl
         this.facets = ((FacetsImpl) value);
     }
 
+	@Override
+	public void init(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, null, null);
+
+        while (parser.nextTag() == XmlPullParser.START_TAG) {
+        	String name = parser.getName();
+        	
+        	if (name.equals("num-results")) {
+        		setNumResults(XppUtils.getElementValueAsLongFromNode(parser));
+            } else if (name.equals("people")) {
+    			PeopleImpl people = new PeopleImpl();
+    			people.init(parser);
+    			setPeople(people);
+            } else if (name.equals("facets")) {
+    			FacetsImpl facets = new FacetsImpl();
+    			facets.init(parser);
+    			setFacets(facets);
+            } else {
+                // Consume something we don't understand.
+            	LOG.warning("Found tag that we don't recognize: " + name);
+            	XppUtils.skipSubTree(parser);
+            }
+        }
+	}
+
+	@Override
+	public void toXml(XmlSerializer serializer) throws IOException {
+		XmlSerializer element = serializer.startTag(null, "people-search");
+		XppUtils.setElementValueToNode(element, "num-results", getNumResults());
+		if (getPeople() != null) {
+			((PeopleImpl) getPeople()).toXml(serializer);
+		}
+		if (getFacets() != null) {
+			((FacetsImpl) getFacets()).toXml(serializer);
+		}
+		serializer.endTag(null, "people-search");
+	}
 }
