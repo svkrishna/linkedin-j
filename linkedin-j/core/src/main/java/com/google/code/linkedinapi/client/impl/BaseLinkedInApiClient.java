dd9713432b4da794bdbbc0433c0bc1229a6acf8c
Vamsi Krishna Sankarayogi <vsankarayogi@gmail.com>
2011-02-25 09:46:39 +0000

    Fixed encoding problems.

--- linkedin-j/core/src/main/java/com/google/code/linkedinapi/client/impl/BaseLinkedInApiClient.java
+++ linkedin-j/core/src/main/java/com/google/code/linkedinapi/client/impl/BaseLinkedInApiClient.java
@@ -17,14 +17,15 @@
 package com.google.code.linkedinapi.client.impl;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
@@ -89,7 +90,10 @@ public abstract class BaseLinkedInApiClient implements LinkedInApiClient {
 
     /** Field description */
     private static final String GZIP_ENCODING = "gzip";
-
+    
+    /** Field description */
+    private static final Charset UTF_8_CHAR_SET = Charset.forName(ApplicationConstants.CONTENT_ENCODING);
+    
     /** Field description */
     protected static final Set<ProfileField> CONNECTION_FIELDS = ProfileField.valuesForConnections();
 
@@ -2019,7 +2023,7 @@ public abstract class BaseLinkedInApiClient implements LinkedInApiClient {
             }
 
             if (xmlContent != null) {
-                PrintStream out = new PrintStream(new BufferedOutputStream(request.getOutputStream()));
+                PrintWriter out = new PrintWriter(new OutputStreamWriter(request.getOutputStream(), UTF_8_CHAR_SET));
 
                 out.print(xmlContent);
                 out.flush();
